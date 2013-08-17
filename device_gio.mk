bb8a2b253dada3f7215c095a3875b773f6938b90
erikcas <erikcas1972@gmail.com>
2013-08-17 17:57:24 +0200

    changes for gio
    Change-Id: I58b9c0dd3c3ff9d689abd8db882dd248fba7535b

--- device_gio.mk
+++ device_gio.mk
@@ -18,11 +18,18 @@ PRODUCT_PACKAGES += \
     wlan_tool \
     wmiconfig
 
+## Build the 3.0.x Kernel
+# BUILD_WITH_30X_KERNEL := true
+
+# BOARD_WLAN_DEVICE := ath6kl_compat
+
 ## Ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
+    device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle \
+    device/samsung/gio/ramdisk/init.msm7x27.rc:root/init.gt-s5660board.rc \
+    device/samsung/msm7x27-common/ramdisk/fstab.msm7x27:root/fstab.gt-s5660board
 
-# Inherit products (Most specific first)
+## Inherit products (Most specific first)
 # gio blobs > samsung common(device/vendor) > other blobs
 $(call inherit-product, vendor/samsung/gio/vendor_blobs.mk)
 $(call inherit-product, device/samsung/msm7x27-common/common.mk)
